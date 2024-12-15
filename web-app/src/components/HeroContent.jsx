6ed043ed91f039605483123a9365bae767eeee23
aman kumar <ak9031821202@gmail.com>
2024-12-15 09:00:22 +0530

    Add some content

--- web-app/src/components/HeroContent.jsx
+++ web-app/src/components/HeroContent.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function HeroContent() {
-  return <div className="mt-10">Web Developer</div>;
+  return (
+    <div>
+      <div className="mt-10 text-4xl font-bold text-center">Aman Kumar</div>
+      <div className="mt-2 text-[80px] font-bold text-center text-">
+        A Web Developer
+      </div>
+      <div className="px-20">
+        A React developer with expertise in modern web development technologies
+        is skilled in building interactive and responsive applications using
+        React.js and Next.js. Proficient in HTML, CSS, JavaScript, and advanced
+        styling frameworks like Tailwind CSS, Bootstrap, and Material-UI, they
+        craft visually appealing user interfaces. With strong knowledge of Redux
+        (Redux Toolkit), they efficiently manage application state in complex
+        projects. Familiarity with version control systems like Git ensures
+        streamlined collaboration in team environments. Additionally, they
+        possess a foundational understanding of backend technologies, including
+        Node.js, Express.js, and MongoDB, enabling them to integrate frontend
+        applications with server-side APIs. They also have basic exposure to
+        React Native, equipping them to explore mobile app development.
+      </div>
+    </div>
+  );
 }
 
 export default HeroContent;
